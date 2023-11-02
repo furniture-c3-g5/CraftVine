4c387f6eee79c658a29042d56098152682bfeb01
Ayoub-Haj-Husein <ayoubabdulrahmanhusein@gmail.com>
2023-11-02 15:16:21 +0300

    add structur for back_end

--- Server/Routes/routes.js
+++ Server/Routes/routes.js
@@ -1,5 +1,5 @@
 const { Router } = require("express")
-const controller = require("../controllers")
+const controller = require("../controllers/product_images")
 const router = Router()
 
 router.get("/", controller.getProducts)
