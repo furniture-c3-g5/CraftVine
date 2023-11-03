e43a3cc7ecdf7af9c1d899e97da4fef14583c930
rawand-hawwari <rawandh3@gmail.com>
2023-11-03 17:59:31 +0300

    explore products component

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../Components/Landing/Hero'
+import ExploreProducts from '../Components/Landing/ExploreProducts'
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <ExploreProducts />
     </div>
   )
 }
