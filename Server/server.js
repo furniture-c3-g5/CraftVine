1bc6797d9980473c8f00d2b3e1900efa99cae93d
Ehabdhafer <ehabzaferabed@gmail.com>
2023-11-04 09:45:07 +0300

    add oauth

--- Server/server.js
+++ Server/server.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const app = express();
+const session = require("express-session");
+const passport = require("passport");
 app.use(express.json());
 var cors = require("cors");
 app.use(cors());
 
+app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 const user_router = require("./routes/users_route");
+const google_router = require("./routes/google_route");
 
 app.use(user_router);
+app.use(google_router);
 
 app.listen(5000, () => {
   console.log("server running at http://localhost:5000");
