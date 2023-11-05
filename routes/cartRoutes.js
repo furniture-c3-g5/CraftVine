3ceaf5be67cb36c9926e8fd349e471068a9c2097
maha-mohammad01 <mahaalkaabneh1997@gmail.com>
2023-11-05 12:58:44 +0300

    cart

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -4,7 +4,8 @@ const cartController = require('../controllers/cartController');
 const router = express.Router();
 
 router.post('/add-to-cart', cartController.addToCart);
-router.get('/cart', cartController.getCartItems);
+router.get('/custom-path', cartController.getCartItems);
 router.delete('/remove-from-cart', cartController.removeFromCart);
 
 module.exports = router;
+
